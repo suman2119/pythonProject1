409e0e36c1290dfc0aff25714ed66d4a2a761fd7
suman2119 <168227538+suman2119@users.noreply.github.com>
2024-04-27 00:59:40 +0530

    Add files via upload

---
+++ quizques.py
@@ -0,0 +1,78 @@
+{
+    "Question": [
+        "Q1. Where was India first national Museum opened?",
+        "Q2. The world nation 5G mobile network was launched by which country?",
+        "Q3. When was Pravasi Bhartiya Divas held in Varanasi?",
+        "Q4. The green planet in the solar system is?",
+        "Q5. Which of these is the small-scale industry in India?",
+        "Q6. The largest public sector undertaking in the country is?",
+        "Q7. Which of these is the plant important in sericulture?",
+        "Q8. At which place on earth are there days & nights of equal length always?",
+        "Q9. Who among the following laid down for punishment for a person becoming mendicant without making adequate provision for dependent wife and children?",
+        "Q10. The father of Indian missile technology is?"
+    ],
+    "Options": [
+        ["Delhi",
+         "Hyderabad",
+         "Rajasthan",
+         "Mumbai"
+        ],
+        ["Japan",
+         "Asia",
+         "South Korea",
+         "America"
+        ],
+        ["2019",
+         "2017",
+         "2020",
+         "2018"
+        ],
+        ["Mars",
+         "Earth",
+         "Venus",
+         "Uranus"
+        ],
+        ["Jute industry",
+         "Paper Industry",
+         "Paper Industry",
+         "Handloom Industry"
+        ],
+        ["Railways",
+         "Airways",
+         "Roadways",
+         "Iron and Steel Plants"
+        ],
+        ["Cassia",
+         "Mulberry",
+         "Legumes",
+         "Pea"
+        ],
+        ["Prime Meridian",
+         "Poles",
+         "Equator",
+         "Nowhere"
+        ],
+        ["Manu",
+         "Yajnavalkya",
+         "Narada",
+         "Kautilya"
+        ],
+        ["Dr Homi Bhabha",
+         "Dr A.P.J. Abdul Kalam",
+         "Dr U.R. Rao",
+         "Dr Chidambaram"
+        ]
+    ],
+    "Answer": [
+        4,
+        3,
+        1,
+        4,
+        4,
+        1,
+        2,
+        3,
+        4,
+        2
+    ]
+}
